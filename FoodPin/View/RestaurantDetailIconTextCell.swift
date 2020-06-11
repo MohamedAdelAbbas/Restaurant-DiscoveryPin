fc5b89c5e21a7ccb3c9312b9960bd23ddec74c17
Mohamed Adel <m.3del95@gmail.com>
2020-06-11 15:11:35 +0200

    Updating the RestaurantDetailViewController Class

---
+++ FoodPin/View/RestaurantDetailIconTextCell.swift
@@ -0,0 +1,28 @@
+//
+//  RestaurantDetailIconTextCell.swift
+//  FoodPin
+//
+//  Created by Mohamed Adel on 6/11/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailIconTextCell: UITableViewCell {
+
+    
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var shortTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodPin/View/RestaurantDetailTextCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantDetailTextCell.swift
+//  FoodPin
+//
+//  Created by Mohamed Adel on 6/11/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTextCell: UITableViewCell {
+
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
