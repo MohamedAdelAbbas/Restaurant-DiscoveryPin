b46680317a578c7f439fc21c8cd8a961d8c82d48
Mohamed Adel <m.3del95@gmail.com>
2020-06-17 01:04:21 +0200

    Moving onto the Next Text Field by (func textFieldShouldReturn + Add tag) In NewVC

---
+++ FoodPin/Controllers/NewRestaurantController.swift
@@ -0,0 +1,81 @@
+//
+//  NewRestaurantController.swift
+//  FoodPin
+//
+//  Created by Mohamed Adel on 6/17/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class NewRestaurantController: UITableViewController, UITextFieldDelegate {
+
+    // MARK: Outlets
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var nameTextField: RoundedTextField! {
+        didSet {
+            nameTextField.tag = 1
+            nameTextField.becomeFirstResponder()
+            nameTextField.delegate = self
+        }
+    } 
+    @IBOutlet var typeTextField: RoundedTextField! {
+        didSet {
+            typeTextField.tag = 2
+            typeTextField.delegate = self
+        }
+    }
+    @IBOutlet var addressTextField: RoundedTextField! {
+        didSet {
+            addressTextField.tag = 3
+            addressTextField.delegate = self
+        }
+    }
+    @IBOutlet var phoneTextField: RoundedTextField! {
+        didSet {
+            phoneTextField.tag = 4
+            phoneTextField.delegate = self
+        }
+    }
+    @IBOutlet var descriptionTextView: UITextView! {
+        didSet {
+            descriptionTextView.tag = 5
+            descriptionTextView.layer.cornerRadius = 5.0
+            descriptionTextView.layer.masksToBounds = true
+        }
+    }
+    // MARK: Properties
+    
+    // MARK: View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    // MARK: Action
+    
+    // MARK: - UITextFieldDelegate methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextTextField = view.viewWithTag(textField.tag + 1) {
+            textField.resignFirstResponder()
+            nextTextField.becomeFirstResponder()
+        }
+        return true
+    }
+    // MARK: Self Defined Methods
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+}
