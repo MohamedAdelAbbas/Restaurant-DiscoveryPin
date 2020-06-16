927c27a24cdeacffedcd012fcddd64a7e704827f
Mohamed Adel <m.3del95@gmail.com>
2020-06-16 14:37:15 +0200

    Add in ReviewVC Fade-in animation with delay

--- FoodPin/Controllers/ReviewViewController.swift
+++ FoodPin/Controllers/ReviewViewController.swift
@@ -11,13 +11,34 @@ import UIKit
 class ReviewViewController: UIViewController {
     
     @IBOutlet var backgroundImageView: UIImageView!
-    
+    @IBOutlet var rateButtons: [UIButton]!
     var restaurant = Restaurant()
     
     override func viewDidLoad() {
         super.viewDidLoad()
        SetupBlurEffect()
-        
+        // Make the button invisible
+        for rateButton in rateButtons {
+        rateButton.alpha = 0
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
+            self.rateButtons[0].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+            self.rateButtons[1].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
+            self.rateButtons[2].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
+            self.rateButtons[3].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
+            self.rateButtons[4].alpha = 1.0
+        }, completion: nil)
     }
     func SetupBlurEffect(){
         backgroundImageView.image = UIImage(named: restaurant.image)
